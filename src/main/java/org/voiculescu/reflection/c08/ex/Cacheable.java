b09043562ceffd9cd4a1703a2148d48472c4a784
cvoiculescu <corneliu@voiculescu.org>
2023-08-02 09:16:23 +0300

    Dynamic Caching implementation

---
+++ src/main/java/org/voiculescu/reflection/c08/ex/Cacheable.java
@@ -0,0 +1,11 @@
+package org.voiculescu.reflection.c08.ex;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Cacheable {
+}
